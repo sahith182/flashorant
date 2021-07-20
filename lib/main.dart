955c08483c29481820af2b49969db9e5e3dcbe1f
Sahith182 <sahithvembuluru@gmail.com>
2021-07-20 22:33:55 +0530

    Removed Comments

--- lib/main.dart
+++ lib/main.dart
@@ -42,8 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Text(
           'Yoru pachi noob',
         ),
