e859819ca986d85a0d6544cc7f0b0d6e134c1539
18Srikar <dsaisrikar@gmail.com>
2021-07-21 22:05:59 +0530

    Given properties to ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flashorant/screens/home_page.dart';
+import 'package:flashorant/screens/splash_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +10,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primaryColor: Color(0xffA50002),
+        scaffoldBackgroundColor: Color(0xff0F1923),
+        textTheme: TextTheme(
+          bodyText1: TextStyle(
+            color: Colors.white,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flashorant Survey'),
+      home: SplashFlash(title: 'Flashorant Survey'),
     );
   }
 }
